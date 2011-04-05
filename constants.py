c2a9216269c9e5794681da7e5724f96203ec17f6
Kevin L. Mitchell <klmitch@einstein.(none)>
2011-04-05 17:20:04 -0500

    Document constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,14 @@
+"""
+==============
+Test Constants
+==============
+
+This module contains the various constants used by the test framework.
+The constants are the various states that a test may be in (RUNNING,
+FAIL, XFAIL, ERROR, DEPFAIL, OK, UOK, and SKIPPED) and the origins of
+messages in the result (PRE, POST, and TEST).
+"""
+
 # Test states
 RUNNING = 'RUNNING'  # test running
 FAIL = 'FAIL'        # test failed
@@ -9,7 +20,7 @@ UOK = 'UOK'          # test unexpectedly completed successfully
 SKIPPED = 'SKIPPED'  # test was skipped
 
 
-# Result origins
+# Result message origins
 PRE = 'PRE'    # Error in pre-execute fixture
 POST = 'POST'  # Error in post-execute fixture
 TEST = 'TEST'  # Error from the test itself
