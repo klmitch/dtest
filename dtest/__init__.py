ecdec9e47334171b8b15c536503b8e8d6f3ebd34
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2011-04-07 14:18:57 -0500

    Forgot to make main() visible

--- dtest/__init__.py
+++ dtest/__init__.py
@@ -42,7 +42,8 @@ Note that both dtest and dtest.util are safe for use with "import *".
 from dtest.capture import Capturer
 from dtest.constants import *
 from dtest.exceptions import DTestException
-from dtest.core import DTestOutput, run, explore, optparser, opts_to_args
+from dtest.core import DTestOutput, run, explore, main, \
+    optparser, opts_to_args
 from dtest.test import istest, nottest, skip, failing, attr, depends, \
     raises, timed, DTestCase, dot
 
@@ -51,6 +52,7 @@ __all__ = ['Capturer',
            'RUNNING', 'FAIL', 'XFAIL', 'ERROR', 'DEPFAIL', 'OK', 'UOK',
            'SKIPPED',
            'DTestException',
-           'DTestOutput', 'run', 'explore', 'optparser', 'opts_to_args',
+           'DTestOutput', 'run', 'explore', 'main',
+           'optparser', 'opts_to_args',
            'istest', 'nottest', 'skip', 'failing', 'attr', 'depends',
            'raises', 'timed', 'DTestCase', 'dot']
