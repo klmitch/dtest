b14aa6a44411a17309d74accfbb3db5c2ffafecf
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2011-04-08 16:46:05 -0500

    Just add a couple of comments to make it clear what we're testing

--- tests/test_alternate.py
+++ tests/test_alternate.py
@@ -2,6 +2,7 @@ from dtest import *
 from dtest.util import *
 
 
+# Ensure that the alternate setUp/tearDown decorators work
 class TestAlternate(DTestCase):
     alternate = None
 
@@ -12,13 +13,16 @@ class TestAlternate(DTestCase):
     def tearDown(self):
         assert_false(self.alternate)
 
+    # Should use the default setUp/tearDown
     def test1(self):
         assert_false(self.alternate)
 
+    # Have to use @istest here to make the decorators available
     @istest
     def test2(self):
         assert_true(self.alternate)
 
+    # Alternate setUp/tearDown for test2
     @test2.setUp
     def alternateSetUp(self):
         assert_is_none(self.alternate)
