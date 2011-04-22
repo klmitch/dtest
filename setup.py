62a0976afdc17b357318c505ef5f360db97b77e7
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2011-04-22 17:09:34 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
     name='DTest',
-    version='0.3',
+    version='0.3.1',
     description="Dependency-based Threaded Test Framework",
     author="Kevin L. Mitchell",
     author_email="kevin.mitchell@rackspace.com",
